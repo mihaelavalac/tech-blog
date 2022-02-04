a3dbf3c0ad795260e2e561516fd36ca1a03bca44
Mihaela Valac <mvalac@student.cscc.edu>
2022-02-04 17:10:06 -0500

    Created the post model with all its routes.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,9 @@
 //Serve as means to collect all the api routes and package them up for use. 
 const router = require('express').Router();
-
 const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes.js');
 
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
