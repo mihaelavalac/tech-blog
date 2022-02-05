af85a927ab9cb316e380b820ed2a71865b6fc6c0
Mihaela Valac <mvalac@student.cscc.edu>
2022-02-04 22:19:46 -0500

    Creted the comment model with all its routes.

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { User, Post, Vote } = require("../../models");
+const { User, Post, Vote, Comment } = require("../../models");
 
 // GET /api/users
 router.get("/", (req, res) => {
@@ -23,14 +23,23 @@ router.get("/:id", (req, res) => {
     include: [
       {
         model: Post,
-        attributes: ['id', 'title', 'post_body', 'created_at']
+        attributes: ["id", "title", "post_body", "created_at"],
+      },
+      // include the Comment model here:
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "created_at"],
+        include: {
+          model: Post,
+          attributes: ["title"],
+        },
       },
       {
         model: Post,
-        attributes: ['title'],
+        attributes: ["title"],
         through: Vote,
-        as: 'voted_posts'
-      }
+        as: "voted_posts",
+      },
     ],
   })
     .then((dbUserData) => {
