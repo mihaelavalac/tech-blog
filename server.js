9dd8d3c1c70d04bbb0efb41e4f22941ad9e36f3c
Mihaela Valac <mvalac@student.cscc.edu>
2022-02-05 21:04:08 -0500

    Created tests to change the date format.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers/');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -19,7 +20,7 @@ const sess = {
 };
 app.use(session(sess));
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
